1466c19b7805e1d50d3c0f48cbe3a258dc466b04
bobcermak <bohuslavcermak62@gmail.com>
2025-05-16 13:42:54 +0200

    fix: update layout and styling for massage sections; enhance swiper component design

--- src/scripts/swiper.js
+++ src/scripts/swiper.js
@@ -5,6 +5,8 @@ import '/src/styles/swiper.css';
 //Logic
 const swiper = new Swiper('.swiper', {
     modules: [Navigation, Pagination, Autoplay],
+    slidesPerView: 1,
+    spaceBetween: 40,
     loop: true,
     pagination: {
         clickable: true,
@@ -15,7 +17,7 @@ const swiper = new Swiper('.swiper', {
         prevEl: '.swiper-button-prev',
     },
     autoplay: {
-        delay: 10000,
+        delay: 12000,
         disableOnInteraction: true,
     },
 });
