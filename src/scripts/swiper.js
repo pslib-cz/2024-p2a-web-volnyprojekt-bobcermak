c453a99916f4baf0a05e30152c624159d434c317
bobcermak <bohuslavcermak62@gmail.com>
2025-06-01 20:43:43 +0200

    fix: refactor swiper component classes for improved structure and styling in index.html, swiper.js, and swiper.css

--- src/scripts/swiper.js
+++ src/scripts/swiper.js
@@ -3,22 +3,24 @@ import { Navigation, Pagination, Autoplay } from 'swiper/modules';
 //CSS
 import '/src/styles/swiper.css';
 
-//Logic
-const swiper = new Swiper('.swiper', {
+//page__services - swiper
+const swiper = new Swiper('.page__services-container', {
     modules: [Navigation, Pagination, Autoplay],
     slidesPerView: 1,
     spaceBetween: 40,
     loop: true,
     pagination: {
         clickable: true,
-        el: '.swiper-pagination',
+        el: '.page__services-container-swiper-pagination',
     },
     navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
+        nextEl: '.page__services-container-swiper-button-next',
+        prevEl: '.page__services-container-swiper-button-prev',
     },
     autoplay: {
         delay: 12000,
         disableOnInteraction: true,
     }
 });
+
+//page__reviews - swiper
